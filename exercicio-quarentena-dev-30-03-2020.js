c3ad6808fcb12ba990e242d196ea9a41a8dc9e65
Run4thewin <35710384+Run4thewin@users.noreply.github.com>
2020-03-30 11:27:16 -0300

    ADIÇÃO DO DESAFICO QUE ESQUECI

--- exercicio-quarentena-dev-30-03-2020.js
+++ exercicio-quarentena-dev-30-03-2020.js
@@ -18,4 +18,18 @@ alert(22 * 365);
 
 //TUDO QUE VAI SER EXIBIDO COMO TEXTO DEVE ESTAR ENTRE ASPAS DUPLAS
 //ALERTA COM FRASE
-alert("Ola, a quanrentena DEV começou");
+alert("Ola, a quanrentena DEV começou")
+
+
+// ==================== DESAFIO ==================== //  
+batimentoPorMinuto = 75;
+minutos = 60  ;
+horas = 24;
+diasNoAno = 365;
+idade = 2;
+
+//CALCULAR QUANTIDADE DE BATIMENTOS
+totalDeBatimentos = batimentoPorMinuto * minutos * horas * diasNoAno * idade;
+
+//ALERTA O RESULTADO
+alert("Eu tenho " + idade + " anos e meus batimentos foram " + totalDeBatimentos)
